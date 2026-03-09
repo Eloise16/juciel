eaec1e1cd74c07b8fef40d191c9f3a71c0857ffa
Eloise16 <eloise.assis.silva@escola.pr.gov.br>
2026-03-09 09:31:36 -0300

    Add files via upload

---
+++ exemplo1.py
@@ -0,0 +1,11 @@
+numero= input ("digite um numero")
+umero= int (numero)
+valida_poitivo= numero>0
+valida_negativo= numero>0
+if valida_poitivo:
+   print ("O numero e positivo")
+elif valida_negativo:
+   print ("numero e negativo")
+else:
+   print("O numero e negativo")
+
